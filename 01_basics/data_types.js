70234ab1c6dab0d823a237873b4d87795678a4b1
Sandesh Chapagain <sandeshchapagain99@gmail.com>
2025-06-11 12:33:18 +0000

    data types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,16 @@
+"use strict";// treat all JS code as newer version 
+// alert(3+3) // we are using node JS, not browser 
+let logic_value = true;
+let age = 18;
+let b = 'b';
+let a = "String";// use double quotes for string 
+console.log(3+3);
+console.log('A');
+console.log("Sandesh");
+console.log(age);
+console.log(typeof 200)
+console.log(typeof 19.27)
+console.log(typeof "hello")
+console.log(typeof true)
+console.log(typeof 19293948483838483939939293889929292929933838383838383849494)
+console.log(typeof 19293948483838483939939293889929292929933838383838383849494n)
